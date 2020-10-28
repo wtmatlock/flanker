b13b12488f01ab49018d56a7ef7e5f5dcde258ed
samuel.lipworth <s.i.lipworth@gmail.com>
2020-10-28 12:09:53 +0000

    update graphs script

--- Rscripts/graphs.R
+++ Rscripts/graphs.R
@@ -1,30 +1,44 @@
-##R script to take output from mash distance matrix and turn into png files for each window 
-
 library(tidyverse)
 
 library(igraph)
-out=NULL
+
 z=1
-graphs=list()
-for(i in seq(100,5000,100)){
+# load some metadata (in this case the source of isolate eg human/cow etc)
+source<-read_csv('../REHAB_isolate_source.csv')
+library(RColorBrewer)
+
+for(i in seq(100,3000,100)){
   print(i)
   q<-read.delim(paste('~/gn/flanker/mash_',i,'.tsv',sep=''),sep='\t',header=F,stringsAsFactors = F)
   names(q)<-c('X1','X2','mash_dist','X4','X5')
   x<-unique(unlist(q[c(1,2)]))
-  x<-str_replace_all(x,'.fasta','')
+  x<-data.frame(str_replace_all(x,'.fasta',''))
+  names(x)<-c('X1')
+  x$name<-str_replace_all(x$X1,'_.*','')
+  x<-left_join(x,source,by=c("name"="Alternative isolate name"))
+  x$Niche<-ifelse(is.na(x$Niche),'human',x$Niche)
+  x$Niche<-ifelse(grepl('WTP',x$Niche),'WTP',x$Niche)
   q$X1<-str_replace_all(q$X1,'.fasta.*','')
   q$X2<-str_replace_all(q$X2,'.fasta.*','')
-  q<-select(q,X1,X2,mash_dist)
-  names(q)<-c('X1','X2','weights')
-  q<-filter(q, weights == 0)
+  q<-filter(q, mash_dist == 0)
+  q<-select(q,X1,X2)
+  names(q)<-c('X1','X2')
+ 
   q<-filter(q, X1 != X2)
-  q<-graph_from_data_frame(q,vertices = x)
-  png(filename = paste(i,'.png'),width = .1,height = .1)
-  plot(q)
+
+  q<- q %>%  rowwise() %>%
+    mutate(HH=paste0(sort(c(X1,X2)), collapse =',')) %>%
+    distinct(HH, .keep_all = TRUE) %>% select(-HH)
+  q<-graph_from_data_frame(q,vertices = x,directed =F )
+  V(q)$color<-ifelse(V(q)$Niche =='human','blue',ifelse(V(q)$Niche=='WTP','lightblue',ifelse(V(q)$Niche=='Cattle','brown',ifelse(V(q)$Niche=='Pig','pink',ifelse(V(q)$Niche=='Sheep','yellow','black')))))
+  png(filename = paste(sprintf("out_%03d",z),'.png',sep=''),width = 800,height = 800)
+  
+  plot(q,vertex.label=NA,vertex.size=2.0,width=0.1,main=i)
+
+  legend("bottomleft",legend=c("human","WTP","Catlle","Pig","Sheep","Other"),fill=c("blue","lightblue","brown","pink","yellow","black"))
   dev.off()
+  z=z+1
   
 }
 
 
-# these png files can the be passed to ffmpeg eg:
-# ffmpeg -framerate 1 -i out_%03d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
