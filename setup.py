b9e0aa2a1e1fa5631b533fab3b607d68b2238b21
William Matlock <55752443+wtmatlock@users.noreply.github.com>
2021-01-28 12:01:36 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import flanker
+
+from setuptools import setup
+
+
+setup(
+    name = 'flanker',
+    version = flanker.__version__,
+    description = ' Gene-flank analysis tool ',
+    url = 'https://github.com/wtmatlock/flanker',
+    license = '',
+    packages=['flanker'],
+    python_requires='>=3.6',
+    install_requires=['pandas', 'biopython', 'pytest'],
+    entry_points = {'console_scripts':['flanker=flanker.flanker:main']},
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English'])
