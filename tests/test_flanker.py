ec1adfc29932a764488465746f5eddc624b0a99e
Bede Constantinides <1007616+bede@users.noreply.github.com>
2021-02-19 19:08:25 +0000

    fixes test

--- tests/test_flanker.py
+++ tests/test_flanker.py
@@ -20,4 +20,4 @@ def run(cmd, cwd=cwd):  # Helper for CLI testing
 
 def test_cli_blactxm():
 	run('flanker -i RHB02-C01_4.fasta -g blaCTX-M-55', cwd=data_dir)
-	run('rm RHB02-C01_4_blaCTX-M-55_1000_both_flank.fasta RHB02-C01_4.fasta_resfinder', cwd=data_dir)  # Tidy up
+	run('rm RHB02-C01_4_blaCTX-M-55_1_1000_both_flank.fasta RHB02-C01_4.fasta_resfinder', cwd=data_dir)  # Tidy up
